e9b1ec5a6fa3f1e451df21ec2539f50456161430
clj <cljqqyx@qq.com>
2018-03-28 10:04:15 +0800

    first

--- currency.py
+++ currency.py
@@ -4,12 +4,18 @@ from trade import *
 
 
 def buycurrency():
-    buy(QTUMSYMBOL, QTUMAMOUNT, QTUMTABLE)
-    buy(EOSSYMBOL, EOSAMOUNT, EOSTABLE)
+    try:
+        buy(QTUMSYMBOL, QTUMAMOUNT, QTUMTABLE)
+        buy(EOSSYMBOL, EOSAMOUNT, EOSTABLE)
+    except:
+        pass
     return ''
 
 
 def sellcurrency():
-    sell(QTUMSYMBOL, QTUMPERCENT, QTUMTABLE)
-    sell(EOSSYMBOL, EOSPERCENT, EOSTABLE)
+    try:
+        sell(QTUMSYMBOL, QTUMPERCENT, QTUMTABLE)
+        sell(EOSSYMBOL, EOSPERCENT, EOSTABLE)
+    except:
+        pass
     return ''
