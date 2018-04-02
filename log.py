1e18d1ff8713149abe4e61bc86bdbf7854bc4c69
clj <cljqqyx@qq.com>
2018-04-02 11:26:30 +0800

    first

--- log.py
+++ log.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from logging.handlers import RotatingFileHandler
+
 
 log=logging.getLogger()
 log.setLevel(logging.WARN)
 fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-h = logging.FileHandler(filename='dthblog.log', encoding='UTF-8')
+h = RotatingFileHandler(filename='dthblog.log', maxBytes=50000000, backupCount=3, encoding='UTF-8')
 h.setFormatter(fmt)
 log.addHandler(h)
 
