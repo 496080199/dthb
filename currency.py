2f915d59a210641f98a82f0ff939149b24d67e4e
clj <cljqqyx@qq.com>
2018-06-22 16:21:47 +0800

    first

--- currency.py
+++ currency.py
@@ -5,53 +5,42 @@ from fxhgbi import *
 
 
 def buycurrency():
-    try:
-        if belowavg():
-            log.warn('低于300日均线买入')
-            buy(QTUMSYMBOL, QTUMAMOUNT * 2, QTUMTABLE)
-            buy(EOSSYMBOL, EOSAMOUNT * 2, EOSTABLE)
-            buy(NEOSYMBOL, NEOAMOUNT * 2, NEOTABLE)
-        elif overavg():
-            log.warn('超高于300日均线不买入')
-            pass
-        else:
-            log.warn('正常买入')
-            buy(QTUMSYMBOL, QTUMAMOUNT, QTUMTABLE)
-            buy(EOSSYMBOL, EOSAMOUNT, EOSTABLE)
-            buy(NEOSYMBOL, NEOAMOUNT, NEOTABLE)
-    except Exception as e:
-
-        log.warn('买入异常退出:'+str(e))
+    if belowavg():
+        log.warn('低于300日均线买入')
+        buy(QTUMSYMBOL, QTUMAMOUNT * 2, QTUMTABLE)
+        buy(EOSSYMBOL, EOSAMOUNT * 2, EOSTABLE)
+        buy(NEOSYMBOL, NEOAMOUNT * 2, NEOTABLE)
+    elif overavg():
+        log.warn('超高于300日均线不买入')
         pass
+    else:
+        log.warn('正常买入')
+        buy(QTUMSYMBOL, QTUMAMOUNT, QTUMTABLE)
+        buy(EOSSYMBOL, EOSAMOUNT, EOSTABLE)
+        buy(NEOSYMBOL, NEOAMOUNT, NEOTABLE)
+
     return ''
 
 
 def sellcurrency():
-    try:
-        sell(QTUMSYMBOL, QTUMPERCENT, QTUMTABLE)
-        sell(EOSSYMBOL, EOSPERCENT, EOSTABLE)
-        sell(NEOSYMBOL, NEOPERCENT, NEOTABLE)
-        sell(ETHSYMBOL, ETHPERCENT, ETHTABLE)
-        sell(XRPSYMBOL, XRPPERCENT, XRPTABLE)
-    except Exception as e:
-        log.warn('卖出异常退出:'+str(e))
-        pass
+    sell(QTUMSYMBOL, QTUMPERCENT, QTUMTABLE)
+    sell(EOSSYMBOL, EOSPERCENT, EOSTABLE)
+    sell(NEOSYMBOL, NEOPERCENT, NEOTABLE)
+    sell(ETHSYMBOL, ETHPERCENT, ETHTABLE)
+    sell(XRPSYMBOL, XRPPERCENT, XRPTABLE)
+
     return ''
 
 
 def hardbuycurrency():
-    try:
-        if belowavg():
-            log.warn('低于300日均线买入')
-            buy(ETHSYMBOL, ETHAMOUNT*2, ETHTABLE)
-            buy(XRPSYMBOL, XRPAMOUNT*2, XRPTABLE)
-        elif overavg():
-            log.warn('超高于300日均线不买入')
-            pass
-        else:
-            log.warn('正常买入')
-            buy(ETHSYMBOL, ETHAMOUNT, ETHTABLE)
-            buy(XRPSYMBOL, XRPAMOUNT, XRPTABLE)
-    except Exception as e:
-        log.warn('硬买入异常退出:'+str(e))
+    if belowavg():
+        log.warn('低于300日均线买入')
+        buy(ETHSYMBOL, ETHAMOUNT*2, ETHTABLE)
+        buy(XRPSYMBOL, XRPAMOUNT*2, XRPTABLE)
+    elif overavg():
+        log.warn('超高于300日均线不买入')
         pass
+    else:
+        log.warn('正常买入')
+        buy(ETHSYMBOL, ETHAMOUNT, ETHTABLE)
+        buy(XRPSYMBOL, XRPAMOUNT, XRPTABLE)
--- trade.py
+++ trade.py
@@ -6,83 +6,90 @@ from log import log
 
 def buy(symbol,amount,table):
     log.warn(getdatetime()+'=='+str(symbol)+'==开始执行买入任务...')
-    exchange = login()
-    exchange.options['createMarketBuyOrderRequiresPrice'] = False
-    orderdata = exchange.create_market_buy_order(symbol=symbol, amount=amount)
-    time.sleep(5)
-    if orderdata['info']['status'] != 'ok':
-        exchange.cancel_order(orderdata['id'])
-        log.warn('订单取消')
-        return 'False'
-    orderinfo = exchange.fetch_order(symbol=symbol, id=orderdata['id'])
-    if orderinfo['status'] != 'closed':
-        exchange.cancel_order(orderdata['id'])
-        log.warn('订单取消')
-        return 'False'
-    filledamount = float(orderinfo['info']['field-amount']) - float(orderinfo['info']['field-fees'])
-    sqldata = "INSERT INTO "+str(table)+" (id,dt,symbol,side,amount,filled,process) VALUES ('" + str(
-        orderinfo['id']) + "','" + str(orderinfo['datetime']) + "','" + str(orderinfo['symbol']) + "','" + str(
-        orderinfo['side']) + "','" + str(orderinfo['amount']) + "','" + str(filledamount) + "','0')"
-    conn = opensqlconn()
-    c = conn.cursor()
-    c.execute(sqldata)
-    conn.commit()
-    conn.close()
-    log.warn(getdatetime() +'=='+str(symbol)+'==买入成功')
+    try:
+        exchange = login()
+        exchange.options['createMarketBuyOrderRequiresPrice'] = False
+        orderdata = exchange.create_market_buy_order(symbol=symbol, amount=amount)
+        time.sleep(5)
+        if orderdata['info']['status'] != 'ok':
+            exchange.cancel_order(orderdata['id'])
+            log.warn('订单取消')
+            return 'False'
+        orderinfo = exchange.fetch_order(symbol=symbol, id=orderdata['id'])
+        if orderinfo['status'] != 'closed':
+            exchange.cancel_order(orderdata['id'])
+            log.warn('订单取消')
+            return 'False'
+        filledamount = float(orderinfo['info']['field-amount']) - float(orderinfo['info']['field-fees'])
+        sqldata = "INSERT INTO "+str(table)+" (id,dt,symbol,side,amount,filled,process) VALUES ('" + str(
+            orderinfo['id']) + "','" + str(orderinfo['datetime']) + "','" + str(orderinfo['symbol']) + "','" + str(
+            orderinfo['side']) + "','" + str(orderinfo['amount']) + "','" + str(filledamount) + "','0')"
+        conn = opensqlconn()
+        c = conn.cursor()
+        c.execute(sqldata)
+        conn.commit()
+        conn.close()
+        log.warn(getdatetime() +'=='+str(symbol)+'==买入成功')
+        return 'True'
+    except Exception as e:
+        log.warn('买入异常退出:'+str(e))
     try:
         del exchange
     except:
         pass
-    return 'True'
+    return 'False'
 
 
 def sell(symbol,percent,table):
     log.warn(getdatetime()+'=='+str(symbol)+'==开始执行卖出任务...')
-    exchange = login()
-    sqldata = "SELECT id,amount,filled  from "+str(table)+" WHERE process='0' AND symbol='"+str(symbol)+"'"
-    conn = opensqlconn()
-    c = conn.cursor()
-    c.execute(sqldata)
-    sqlresult = c.fetchall()
-    conn.commit()
-    sumfilled = Decimal(0.0)
-    sumamount = Decimal(0.0)
-    idlist = []
-    for row in sqlresult:
-        sumfilled += row[2]
-        sumamount += row[1]
-        idlist.append(row[0])
-    log.warn('总成本:' + str(sumamount))
-    log.warn('总数量:' + str(sumfilled))
-    log.warn('关联单:' + str(len(idlist)))
-    wantprofit = ((Decimal(percent) / 100) + 1) * sumamount
-    orderbook = exchange.fetch_order_book(symbol=symbol)
-    bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
-    ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
-    averageprice = Decimal((ask + bid) / 2)
-    sumfilled = sumfilled * Decimal(0.98)
-    profit = averageprice * sumfilled
-    if sumfilled == Decimal(0):
-        profitprice=Decimal(0)
-    else:
-        profitprice = wantprofit / sumfilled
-    log.warn('当前均价:' + str(averageprice) + ',卖出数量:' + str(sumfilled) + ',当前收益:' + str(profit) + ',预期收益:' + str(wantprofit) + ',预期均价:' + str(profitprice))
-    if profit > wantprofit:
-        try:
-            exchange.create_market_sell_order(symbol=symbol, amount=sumfilled)
-        except:
-            log.warn('订单异常')
-            return 'False'
-        for oid in idlist:
-            sqldata = "UPDATE "+str(table)+" set process = '1' WHERE id='" + str(oid) + "' AND symbol='"+str(symbol)+"'"
-            c.execute(sqldata)
-            conn.commit()
-            log.warn('已更新订单' + str(oid) + '的状态')
+    try:
+        exchange = login()
+        sqldata = "SELECT id,amount,filled  from "+str(table)+" WHERE process='0' AND symbol='"+str(symbol)+"'"
+        conn = opensqlconn()
+        c = conn.cursor()
+        c.execute(sqldata)
+        sqlresult = c.fetchall()
+        conn.commit()
+        sumfilled = Decimal(0.0)
+        sumamount = Decimal(0.0)
+        idlist = []
+        for row in sqlresult:
+            sumfilled += row[2]
+            sumamount += row[1]
+            idlist.append(row[0])
+        log.warn('总成本:' + str(sumamount))
+        log.warn('总数量:' + str(sumfilled))
+        log.warn('关联单:' + str(len(idlist)))
+        wantprofit = ((Decimal(percent) / 100) + 1) * sumamount
+        orderbook = exchange.fetch_order_book(symbol=symbol)
+        bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+        ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+        averageprice = Decimal((ask + bid) / 2)
+        sumfilled = sumfilled * Decimal(0.98)
+        profit = averageprice * sumfilled
+        if sumfilled == Decimal(0):
+            profitprice=Decimal(0)
+        else:
+            profitprice = wantprofit / sumfilled
+        log.warn('当前均价:' + str(averageprice) + ',卖出数量:' + str(sumfilled) + ',当前收益:' + str(profit) + ',预期收益:' + str(wantprofit) + ',预期均价:' + str(profitprice))
+        if profit > wantprofit:
+            try:
+                exchange.create_market_sell_order(symbol=symbol, amount=sumfilled)
+            except:
+                log.warn('订单异常')
+                return 'False'
+            for oid in idlist:
+                sqldata = "UPDATE "+str(table)+" set process = '1' WHERE id='" + str(oid) + "' AND symbol='"+str(symbol)+"'"
+                c.execute(sqldata)
+                conn.commit()
+                log.warn('已更新订单' + str(oid) + '的状态')
+            conn.close()
+            log.warn(getdatetime() +'=='+str(symbol)+ '==卖出成功')
+            return 'True'
         conn.close()
-        log.warn(getdatetime() +'=='+str(symbol)+ '==卖出成功')
-        return 'True'
-    conn.close()
-    log.warn(getdatetime() +'=='+str(symbol)+'==未达卖出条件')
+        log.warn(getdatetime() +'=='+str(symbol)+'==未达卖出条件')
+    except Exception as e:
+        log.warn('卖出异常退出:'+str(e))
     try:
         del exchange
     except:
