ffdbde4b199d1124f4baf1bfc3a073bd9fb7f1ee
xiaopao <cljqqyx@qq.com>
2018-09-24 15:41:29 +0800

    new

--- currency.py
+++ currency.py
@@ -27,7 +27,7 @@ def sellcurrency():
     sell(EOSSYMBOL, EOSPERCENT, EOSTABLE)
     #sell(NEOSYMBOL, NEOPERCENT, NEOTABLE)
     sell(ETHSYMBOL, ETHPERCENT, ETHTABLE)
-    sell(XRPSYMBOL, XRPPERCENT, XRPTABLE)
+    #sell(XRPSYMBOL, XRPPERCENT, XRPTABLE)
     checkbalance()
     return ''
 
@@ -36,11 +36,11 @@ def hardbuycurrency():
     if belowavg():
         log.warn('低于300日均线买入')
         buy(ETHSYMBOL, ETHAMOUNT*2, ETHTABLE)
-        buy(XRPSYMBOL, XRPAMOUNT*2, XRPTABLE)
+        #buy(XRPSYMBOL, XRPAMOUNT*2, XRPTABLE)
     elif overavg():
         log.warn('超高于300日均线不买入')
         pass
     else:
         log.warn('正常买入')
         buy(ETHSYMBOL, ETHAMOUNT, ETHTABLE)
-        buy(XRPSYMBOL, XRPAMOUNT, XRPTABLE)
+        #buy(XRPSYMBOL, XRPAMOUNT, XRPTABLE)
